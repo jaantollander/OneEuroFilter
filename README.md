abe150ae822e0175a9b876aa19dead894112d476
Jaan <jaan.tollander@gmail.com>
2018-12-29 15:50:13 +0200

    Example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 1€ Filter (One Euro Filter)
+![](one_euro_filter.gif)
+
 A simple Python implementation of the 1€ Filter, aka One Euro Filter.
--- example.py
+++ example.py
@@ -1,45 +1,63 @@
-import os
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+from matplotlib.animation import FuncAnimation
 
 from one_euro_filter import OneEuroFilter
 
-
+# Parameters
 frames = 100
-t = np.linspace(0, 4*np.pi, frames)
-x = np.sin(t) + np.random.normal(scale=0.1, size=len(t))
-x_hat = np.zeros_like(x)
-x_hat[0] = x[0]
-# TODO: tune coefficients
+start = 0
+end = 4 * np.pi
+scale = 0.05
+
+
+# The noisy signal
+t = np.linspace(start, end, frames)
+x = np.sin(t)
+x_noisy = x + np.random.normal(scale=scale, size=len(t))
+
+
+# The filtered signal
+min_cutoff = 0.004
+beta = 0.7
+x_hat = np.zeros_like(x_noisy)
+x_hat[0] = x_noisy[0]
 one_euro_filter = OneEuroFilter(
-    t[0], x[0],
-    min_cutoff=0.004,
-    beta=0.005
+    t[0], x_noisy[0],
+    min_cutoff=min_cutoff,
+    beta=beta
 )
 for i in range(1, len(t)):
-    x_hat[i] = one_euro_filter(t[i], x[i])
+    x_hat[i] = one_euro_filter(t[i], x_noisy[i])
 
 
 # The figure
 # https://eli.thegreenplace.net/2016/drawing-animated-gifs-with-matplotlib/
-fig, ax = plt.subplots()
+seaborn.set()
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.set(
+    xlim=(start, end),
+    ylim=(1.1*(-1-scale), 1.1*(1+scale)),
+    xlabel="$t$",
+    ylabel="$x$",
+)
 fig.set_tight_layout(True)
-signal, _ = ax.plot(t[0], x[0], 'o')
-filtered, _ = ax.plot(t[0], x_hat[0], '-')
+signal, = ax.plot(t[0], x_noisy[0], 'o')
+filtered, = ax.plot(t[0], x_hat[0], '-')
+
 
 def update(i):
     print(i)
-    signal.set_data(t[0:i], x[0:i])
-    filtered.set_data(t[0:i], x_hat[0:t])
+    signal.set_data(t[0:i], x_noisy[0:i])
+    filtered.set_data(t[0:i], x_hat[0:i])
     return signal, filtered
 
 
 if __name__ == '__main__':
     # FuncAnimation will call the 'update' function for each frame; here
     # animating over 10 frames, with an interval of 200ms between frames.
-    anim = FuncAnimation(fig, update, frames=np.arange(0, frames), interval=20)
-    if len(sys.argv) > 1 and sys.argv[1] == 'save':
-        anim.save('signal.gif', dpi=80, writer='imagemagick')
-    else:
-        # plt.show() will just loop the animation forever.
-        plt.show()
+    anim = FuncAnimation(fig, update, frames=frames, interval=100)
+    anim.save('one_euro_filter.gif', dpi=80, writer='imagemagick')
+    # update(frames)
+    # plt.show()
